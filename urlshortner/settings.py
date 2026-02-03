73dda7f4c71a8a9e9ade59d2a3935a145475685b
yjjwal <ujjwalbohora873@gmail.com>
2026-02-03 09:30:17 +0530

    changes

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m#lw26q*=ly-0#b+ta39ie^s-4v-94865^jv8-ojuvgxtfjern'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['urlshortener.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
